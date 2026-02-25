b094532c70915a4bd17b033237d6583e1802fb28
Bungcwalisa <magobiyanebungcwalisa@gmail.com>
2026-02-25 10:06:09 +0200

    yes

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,31 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
-import apiRoutes from './routes/apiRoutes.js';
-import authenticateToken from './middleware/authMiddleware.js';
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import authRoutes from './routes/authRoutes.js'
+import apiRoutes from './routes/apiRoutes.js'
+import authenticateToken from './middleware/authMiddleware.js'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/auth', authRoutes);
-app.use('/api', apiRoutes);
+app.get('/', (req, res) => {
+  res.send('Server is working')
+})
+
+app.use('/api/auth', authRoutes)
+app.use('/api', apiRoutes)
 
 app.get('/api/protected', authenticateToken, (req, res) => {
-    res.json({ message: `Hello user ${req.user.id}, you are authenticated!` });
-});
+  res.json({ message: `Hello user ${req.user.id}, you are authenticated!` })
+})
 
-const PORT = 5173;
-app.listen(PORT, () => console.log(`Backend running on port ${PORT}`, `http://localhost:${PORT}`));  
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`)
+})
+
+
